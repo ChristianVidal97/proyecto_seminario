b39352d96cb28cd51e582269714d6bf86fb5a148
ChristianVidal97 <www.ccvo@gmail.com>
2023-08-24 22:31:18 -0500

    commit inicial

---
+++ index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>SaludTotal Clínica Médica</title>
+
+  <!-- MDB icon -->
+  <link rel="icon" href="img/logop.png" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="css/style.css">
+
+
+  <!-- <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script> -->
+  <!-- <script type="text/javascript" src="js/funciones.js"></script> -->
+  <!-- jQuery -->
+  <!-- <script type="text/javascript" src="js/jquery.min.js"></script> -->
+  <!-- Bootstrap tooltips -->
+  <!-- <script type="text/javascript" src="js/popper.min.js"></script> -->
+  <!-- Bootstrap core JavaScript -->
+  <!-- <script type="text/javascript" src="js/bootstrap.min.js"></script> -->
+  <!-- MDB core JavaScript -->
+  <!-- <script type="text/javascript" src="js/mdb.min.js"></script> -->
+  <!-- Your custom scripts (optional) -->
+  <!-- <script type="text/javascript"></script> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+</head>
+
+<body style="background-image: url('img/fondoindex.png'); background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
+
+  <header>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar" style="background-color: rgb(1, 63, 42);">
+      <div class="container-fluid">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+
+          <ul class="navbar-nav d-flex justify-content-center">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php"><strong> INICIO</strong></a>
+            </li>
+
+            <li class="nav-item ">
+              <a class="nav-link" href="paciente/index.php"><strong> PACIENTE</strong></a>
+            </li>
+
+            <li class="nav-item ">
+              <a class="nav-link" href="doctor/index.php"><strong> DOCTOR</strong></a>
+            </li>
+
+            <li class="nav-item ">
+              <a class="nav-link" href="administrativos/index.php"><strong> ADMINISTRATIVOS</strong></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+  </header> <br><br><br><br>
+
+  <div align="center" style="color: white">
+    <h1>Bienvenidos</h1>
+    <h2 class="card-title font-weight-bold" ; style="color: white"><a>SaludTotal Clínica Médica</a></h2>
+    <h5 class="card-title font-weight-bold" ; style="color: white"><a><i>"Cuidando tu bienestar, siempre."<i></a>
+    </h5>
+    <img src="img/nuevo.png">
+  </div>
+  <!-- carrucel -->
+  <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="3000">
+        <img src="img/fondo1.jpeg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>#ServimosConElCorazón</h5>
+          <p>Nuestra misíon es la Vida</p>
+        </div>
+      </div>
+
+      <div class="carousel-item" data-bs-interval="3000">
+        <img src="img/img2.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>¡Nos Proyectamos!</h5>
+          <p>hacia un futuro de excelente calidad.</p>
+        </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="3000">
+        <img src="img/img3.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Cirugías programadas en especialidades</h5>
+          <p>CIRUGÍA GENERAl- GINECOBSTETRICIA - CIRUGÍA DE TRAUMATOLOGÍA- NEUROCIRUGÍA</p>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+
+  <br>
+  <!-- TARGETAS -->
+  <div class="container">
+    <!-- Card body -->
+    <div class="card-body">
+      <div class="row">
+        <div class="col">
+          <!-- Card -->
+          <div class="card booking-card" style="height: 750px;">
+            <!-- Card image -->
+            <div class="view overlay">
+              <img class="card-img-top" src="img/donar.png" alt="Card image cap">
+              <div class="mask rgba-white-slight"></div>
+              </a>
+              <br>
+            </div>
+            <!-- Card content -->
+            <div class="card-body text-center">
+              <!-- Title -->
+              <h4 class="card-title font-weight-bold"><a>Campaña de donación de sangre</a></h4>
+              <!-- Data -->
+              <ul class="list-unstyled list-inline rating mb-0">
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"> </i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item">
+                  <p class="text-muted"></p>
+                </li>
+              </ul>
+              <br>
+              <!-- Text -->
+              <h2 class="card-text">Citas Sanitas <br><br>
+
+                Con tu donación de sangre ayudas a salvar 3 vidas, regeneras tu sangre, obtienes un buen análisis de tu
+                estado de salud y disminuyes riesgos de enfermedades cardiovasculares. ¡Gracias por Donar y Regalar
+                Vida! <br>
+              </h2>
+
+              <hr class="my-4">
+              <!-- Button -->
+              <div align="center">
+                <a href="estudiante/index.php" class="btn btn-flat deep-purple-text p-1 mx-0 mb-0">REGISTRARSE</a><br>
+              </div>
+            </div>
+
+          </div>
+          <!-- Card -->
+        </div>
+
+        <div class="col">
+          <!-- Card -->
+          <div class="card booking-card" style="height: 750px;">
+            <!-- Card image -->
+            <div class="view overlay">
+              <img class="card-img-top" src="img/manos.png" alt="Card image cap">
+              <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+            <!-- Card content -->
+            <div class="card-body text-center">
+              <!-- Title -->
+              <h4 class="card-title font-weight-bold"><a>Higiene de manos</a></h4>
+              <!-- Data -->
+              <ul class="list-unstyled list-inline rating mb-0">
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"> </i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item">
+                  <p class="text-muted"></p>
+                </li>
+              </ul>
+              <br>
+              <!-- Text -->
+              <h2 class="card-text">
+                Juntos, aceleramos la acción <br><br>
+                SALVA VIDAS Limpia tus manos<br><br> Día Mundial de la Higiene de las Manos 5 de mayo 2023<br>
+              </h2>
+              <hr class="my-4">
+              <!-- Button -->
+              <div align="center">
+                <a href="https://www.youtube.com/watch?v=NMmAj1EKdVo&ab_channel=ReddeCl%C3%ADnicasSanPablo" class="btn btn-flat deep-purple-text p-1 mx-0 mb-0">Conoce más</a>
+              </div>
+            </div>
+          </div>
+          <!-- Card -->
+        </div>
+
+        <div class="col">
+          <!-- Card -->
+          <div class="card booking-card" style="height: 750px;">
+
+            <!-- Card image -->
+            <div class="view overlay">
+              <img class="card-img-top" src="img/emb.png" alt="Card image cap">
+              <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+            <!-- Card content -->
+            <div class="card-body text-center">
+
+              <!-- Title -->
+              <h4 class="card-title font-weight-bold"><a class="text-center">Linea amiga para maternas y gestantes</a></h4>
+              <!-- Data -->
+              <ul class="list-unstyled list-inline rating mb-0">
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"> </i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+                <li class="list-inline-item">
+                  <p class="text-muted"></p>
+                </li>
+              </ul>
+              <br>
+              <!-- Text -->
+              <h2 class="card-text text-center">La Clínica SaludTotal ha habilitado un número telefónico para atender únicamente a
+                mujeres en estado de embarazo o que crean que pueden estarlo, pues es de suma importancia la captación
+                temprana de estas personas, con el fin de mitigar cualquier complicación de salud en la madre y en el
+                bebé desde el inicio de esta etapa.
+                La denominada “Línea Amiga de la Mujer” está disponible de lunes a viernes de 7:00 AM a 5:00
+                PM</h2><br>
+              <hr>
+              <h2 class="card-text text-center">Call center 317 645 2713</h2><br>
+
+              <!-- Button 
+              <a href="estudiante/index.php" class="btn btn-flat deep-purple-text p-1 mx-0 mb-0">REGISTRARSE</a><br>-->
+            </div>
+          </div>
+          <!-- Card -->
+        </div>
+      </div>
+      <br>
+    </div>
+  </div>
+</body>
+
+
+
+<!-- Footer -->
+<footer class="page-footer font-small white pt-2">
+  <!-- Footer Links -->
+  <div class="text-center">
+
+    <div class="container-fluid pt-3 pb-3" style="background-image: url('img/pieimg.png');">
+      <div class="input-group mb-3" style="color: white">
+        <!-- Content -->
+        <h5>DESDE 1921, ESTAMOS CONTIGO!
+          <hr>
+          Somos el principal referente en salud de la Ex-provincia de Obando, llegando a mas de 300 mil Habitantes con servicios de calidad en el sur Occidente Colombiano. <br>
+
+        </h5> <br>
+      </div>
+
+
+      <div align="center" class="text-light">
+        <img src="imagenes/udenar.png" width="120" style="color: white;">
+      </div>
+    </div>
+    <div style="background-color: rgb(1, 63, 42);">
+      <div class="footer-copyright text-center py-3" style="color: white"> © 2023 <br>
+        <a href="https://mdbootstrap.com/"></a>
+      </div>
+    </div>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+</html>
